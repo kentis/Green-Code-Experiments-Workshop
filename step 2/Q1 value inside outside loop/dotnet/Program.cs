060d5cc7df2734e786027626696036a872edb4d9
Kent Inge <kent.simonsen@tietoevry.com>
2024-06-09 17:33:28 +0200

    Q1 dotnet

---
+++ step 2/Q1 value inside outside loop/dotnet/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+
+public class Program {
+    Stopwatch sw = new Stopwatch();
+    public long inside(){
+        // Initialize variable inside the loop
+        sw.Start();
+        var y=0;
+        for(var i = 0; i < 10_000_000; i++){
+            int z = 1 + 1;
+            y = y + z;
+        }
+        sw.Stop();
+        
+        return sw.ElapsedMilliseconds;
+    }
+
+    public long outside() {
+        // Initialize variable outside the loop
+        sw.Start();
+        var y = 0;
+        int z = 1 + 1;
+        for(var i = 0; i < 10_000_000; i++){
+            y = y + z;
+        }
+        sw.Stop();
+        
+        return sw.ElapsedMilliseconds;
+    }
+
+    public static void Main(){
+        const int numIterations = 10;
+        var program =  new Program();
+        var insideResults = new List<long>(numIterations);
+        var outsideResults = new List<long>(numIterations);
+        //warmup
+       program.inside();
+
+       for(int i = 0; i < numIterations; i++){
+         program.sw.Reset();
+         insideResults.Add(program.inside());
+       }
+
+       Console.WriteLine($"Mean Time taken inside loop: {insideResults.Sum()/(numIterations+0.0)}ms");
+
+        program.outside();
+       for(int i = 0; i < numIterations; i++){
+         program.sw.Reset();
+         outsideResults.Add(program.outside());
+       }
+
+        Console.WriteLine($"Mean Time taken outside loop: {outsideResults.Sum()/(numIterations+0.0)}ms");
+
+
+    }
+}
+
+
+
+
+
+
+
+
