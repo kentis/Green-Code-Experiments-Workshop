9e5cc70f8cedbfb1b84d682760dadf7b9faac8e4
Kent Inge <kent.simonsen@tietoevry.com>
2024-06-14 08:04:49 +0200

    updated string concatination experiment with more string-numbers

--- step 2/Q4 string concatination/dotnet/Program.cs
+++ step 2/Q4 string concatination/dotnet/Program.cs
@@ -9,11 +9,26 @@ class DateParsingBenchmark
     static void Main()
     {
         
-        Console.WriteLine("Two strings");
+        Console.WriteLine("Two strings\n");
         addTwoStrings();
 
-        Console.WriteLine("100");
+        Console.WriteLine("\n100 Strings\n");
         addManyStrings(100);
+
+        Console.WriteLine("\n50 Strings\n");
+        addManyStrings(50);
+
+        Console.WriteLine("\n25 Strings\n");
+        addManyStrings(25);
+        
+        Console.WriteLine("\n12 Strings\n");
+        addManyStrings(12);
+        
+        Console.WriteLine("\n6 Strings\n");
+        addManyStrings(6);
+        
+        Console.WriteLine("\n3 Strings\n");
+        addManyStrings(3);
 
     }
 
@@ -34,7 +49,7 @@ class DateParsingBenchmark
             return a+b+c+d; 
         });
 
-        MeasureSpeed("Add (+) string concationation", first, second, iterations, (firstString, secondString) => {firstString + secondString});
+        MeasureSpeed("Add (+) string concationation", first, second, iterations, (firstString, secondString) => {return firstString + secondString;});
         
         MeasureSpeed("Template strings", first, second, iterations, (firstString, secondString) => $"{firstString}{secondString}");
         
@@ -66,7 +81,7 @@ class DateParsingBenchmark
         });
 
         MeasureSpeed("Add (+) string concationation", first+"", second+"", iterations, (firstString, secondString) => {
-            for(int i = 0; i<numappends){
+            for(int i = 0; i<numappends;i++){
                 firstString += secondString;
             }
             return firstString;
@@ -74,7 +89,7 @@ class DateParsingBenchmark
         
         MeasureSpeed("Template strings", first+"", second+"", iterations, (firstString, secondString) => {
             for(int i = 0; i<numappends;i++){
-                firstString += $"{firstString}{secondString}";
+                firstString = $"{firstString}{secondString}";
             }
             return firstString;
         });
@@ -88,7 +103,11 @@ class DateParsingBenchmark
         });
 
         MeasureSpeed("string.Join", first+"", second+"", iterations, (firstString, secondString) => {
-            string[] array = {firstString,secondString};
+            string[] array = new string[numappends+1];
+            array[0] = firstString;
+            for(int i = 1; i<numappends;i++){
+                array[i] = secondString;
+            }
             return string.Join("",array);
         });
 
